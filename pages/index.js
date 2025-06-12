d1e96caf15d2326f56b9e980928572ed4b6f6d38
samiksha3002 <Samiksha.jaiswal.bca@srwc.raisoni.net>
2025-06-12 22:55:51 +0530

    Head

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import CompanyTrustSection from "./components/CompanyTrustSection";
 import Posters from "./components/Posters";
 import Customer from "./components/Customer";
 import Location from "./components/Location";
+import Header from "./components/Header";
 
 const menuItems = [
   { name: "Home", submenu: ["Subhome 1", "Subhome 2"] },
@@ -30,64 +31,7 @@ export default function Home() {
 
   return (
     <div>
-      <Head>
-        <title>7th Media Icon</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-          integrity="sha512-pap1b6ww+pAfszUgAb4+..."
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-      </Head>
-
-      {/* Header */}
-      <header className="bg-white text-black px-8 py-4 flex justify-between items-center shadow-md z-50 relative">
-        {/* Logo */}
-        <div className="text-2xl font-bold flex items-center space-x-2">
-          <img src="/images/7th.png" alt="logo" className="h-20" />
-        </div>
-
-        {/* Navbar */}
-        <nav className="flex items-center space-x-6 text-xl font-medium">
-          {menuItems.map((item, index) => (
-            <div
-              key={item.name}
-              className="group relative cursor-pointer"
-              onMouseEnter={() => setActiveIndex(index)}
-              onMouseLeave={() => setActiveIndex(null)}
-            >
-              <span className="flex items-center space-x-1">
-                <span>{item.name}</span>
-                {item.submenu && (
-                  <i className="fas fa-chevron-down text-xs ml-1" />
-                )}
-              </span>
-              <span className="absolute top-full left-0 w-full h-[2px] bg-yellow-400 scale-x-0 group-hover:scale-x-100 transition-transform origin-left"></span>
-
-              {activeIndex === index && item.submenu && (
-                <div className="absolute top-full left-0 mt-2 w-40 bg-white rounded-md shadow-lg py-2 z-20">
-                  {item.submenu.map((subItem) => (
-                    <div
-                      key={subItem}
-                      className="px-4 py-2 text-black hover:bg-yellow-400 hover:text-white cursor-pointer"
-                    >
-                      {subItem}
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
-          ))}
-        </nav>
-
-        {/* Social Icons */}
-        <div className="flex space-x-4 text-xl text-black">
-          <i className="fab fa-instagram hover:text-yellow-400 transition" />
-          <i className="fab fa-linkedin hover:text-yellow-400 transition" />
-          <i className="fab fa-facebook hover:text-yellow-400 transition" />
-        </div>
-      </header>
+      <Header />
 
       {/* Hero Section */}
       <section
